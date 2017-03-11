ad140c98afa94aee94348397306c5173e66d3238
rabin <rivansen007@gmail.com>
2017-03-11 15:18:53 +0545

    Except edit all implemented

---
+++ src/app/quote.service.ts
@@ -0,0 +1,41 @@
+
+import {Injectable} from "@angular/core";
+import {Http, Response, Headers} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs";
+
+@Injectable()
+
+export class QuoteService {
+    constructor(private http: Http) {
+    }
+
+    addQuote(content: string){
+        const body = JSON.stringify({content: content});
+        const headers = new Headers({'Content-Type' : 'application/json'});
+        return this.http.post('http://localhost:8000/api/quote', body, {headers: headers})
+    }
+
+    getQuotes(): Observable<any> {
+        return this.http.get('http://localhost:8000/api/quotes')
+            .map(
+                (response: Response) => {
+                    return response.json().quotes;
+                }
+            );
+    }
+
+    updateQuote(id: number, newContent:string){
+        const body = JSON.stringify({content: newContent});
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.put('http://localhost:8000/api/quote/' + id, body, {headers: headers})
+            .map(
+                (response: Response) => response.json()
+            )
+    }
+
+    deleteQuote(id: number){
+        return this.http.delete('http://localhost:8000/api/quote/' + id);
+    }
+}
+  
